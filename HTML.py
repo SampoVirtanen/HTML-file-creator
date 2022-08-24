085ed2a22f609d2aac980254baa98281666a0b73
SampoVirtanen <102716528+SampoVirtanen@users.noreply.github.com>
2022-08-24 15:06:30 +0300

    Now also creates CSS and JS files

--- HTML.py
+++ HTML.py
@@ -1,22 +1,24 @@
 import subprocess
-name=input("Input a name: ")
-name=(name+".html")
-path="C:/Output/" + name
-f = open(path, "x")
-f.write('''<!doctype html>
-
-<html lang="fi">
-<head>
-	<meta charset="utf-8">
-	<title> </title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap-theme.min.css">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
- 
-</body>
-</html>''')
-f.close
-path=path.replace('/', '\\')
-subprocess.Popen(r'explorer /select,"'+path+'"')
+import os
+import shutil
+nimi=input("Anna projektin nimi: ")
+sijainti = "C:/Output/{}".format(nimi)
+if not os.path.exists(sijainti):
+    os.makedirs(sijainti)
+sijainticss = "C:/Output/{}/css".format(nimi)
+if not os.path.exists(sijainticss):
+    os.makedirs(sijainticss)
+sijaintijs = "C:/Output/{}/js".format(nimi)
+if not os.path.exists(sijaintijs):
+    os.makedirs(sijaintijs)
+src_css = "C:/Input/css/bootstrap.min.css"
+dst_css = "C:/Output/{}/css/bootstrap.min.css".format(nimi)
+shutil.copy(src_css, dst_css)
+src_js = "C:/Input/js/bootstrap.min.js"
+dst_js = "C:/Output/{}/js/bootstrap.min.js".format(nimi)
+shutil.copy(src_js, dst_js)
+src_html = "C:/Input/index.html"
+dst_html = "C:/Output/{}/index.html".format(nimi)
+shutil.copy(src_html, dst_html)
+sijainti=sijainti.replace('/', '\\')
+subprocess.Popen(r'explorer /select,"'+sijainti+'"')
